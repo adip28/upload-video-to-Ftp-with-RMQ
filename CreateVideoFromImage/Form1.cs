c7f2024a311a40e4ff7264d03e715d8765b240f2
adip28 <adi.permana60@ymail.com>
2019-11-15 11:41:39 +0700

    first commit

---
+++ CreateVideoFromImage/Form1.cs
@@ -0,0 +1,187 @@
+﻿//using AForge.Video.FFMPEG;
+using AForge.Video.FFMPEG;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+using WMPLib;
+
+namespace CreateVideoFromImage
+{
+    public partial class Form1 : Form
+    {
+        /*const string Path = @"G:\BasukiRachmat\";
+        const string PathDuration = @"G:\BasukiRachmat\abodulmuluk.mp4";
+        const string OutputVideoPath = @"G:\BasukiRachmat\output\";
+        const string VideoPath = @"G:\kuliah\project\PPTIK ITB\ATCS\video_cctv\abodulmuluk.mp4";
+        const string paths = @"G:\kuliah\project\PPTIK ITB\ATCS\video_cctv\abodulmuluk.mp4";*/
+
+        const string BasePath = "D:/ATCS/";
+        //const string BaseInputVideoPath = @"G:\";
+        const string BaseOutputVideoPath = @"D:\ATCS\VideoOutput\";
+        private int _tick = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"config2.txt");
+            foreach (string line in lines)
+            {
+                string[] words = line.Split('|');
+                string directory = words[0];
+                string filename = words[1];
+                string filefullpath = words[2];
+                string pattern = "*.mp4";
+                string InputVideoPath = filefullpath;
+
+                if (Directory.Exists(InputVideoPath))
+                {
+
+                    var dirInfo = new DirectoryInfo(InputVideoPath);
+                    if (dirInfo.GetFiles().Length != 0)
+                    {
+                        var file = (from f in dirInfo.GetFiles(pattern) orderby f.LastWriteTime descending select f).First();
+                        ConvertVideoToImg(file.FullName, directory, filename);
+                        Console.WriteLine(file.FullName);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tidak ada File " + InputVideoPath);
+                    }
+                }
+               
+            }
+
+        }
+
+       
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _tick++;
+            label1.Text = _tick.ToString();
+            if (_tick == 1) {
+                string[] lines = System.IO.File.ReadAllLines(@"config2.txt");
+                foreach (string line in lines)
+                {
+                    string[] words = line.Split('|');
+                    string directory = words[0];
+                    string filename = words[1];
+                    string filefullpath = words[2];
+                    string pattern = "*.mp4";
+                    string InputVideoPath = filefullpath;
+                    /*if (!Directory.Exists(InputVideoPath))
+                        Directory.CreateDirectory(InputVideoPath);*/
+                    var dirInfo = new DirectoryInfo(InputVideoPath);
+                    if (dirInfo.GetFiles().Length != 0)
+                    {
+                        var file = (from f in dirInfo.GetFiles(pattern) orderby f.LastWriteTime descending select f).First();
+                        try
+                        {
+                            ConvertVideoToImg(file.FullName, directory, filename);
+                        }
+                        catch {
+                            Console.WriteLine("Gagal Convert Video" + filename);
+                        }
+                        
+                        Console.WriteLine(file.FullName);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tidak ada File " + InputVideoPath);
+                    }
+
+                }
+                _tick = 0;
+            }
+            
+        }
+
+        public void ConvertVideoToImg(string VideoFile, string directory,string filename)
+        {
+            String BaseImgPath = BasePath + "img/"+filename+"/";
+            if (!Directory.Exists(BaseImgPath))
+                Directory.CreateDirectory(BaseImgPath);
+            //Process.Start("ffmpeg.exe", "-ss 00:06:40 -i "+p+" -t 00:06:50 -c copy out7.avi");
+            //Process.Start("ffmpeg.exe", "-sseof -10 -i out6.avi -c copy output.avi");
+
+            //Process.Start("ffmpeg.exe", "-i "+p+" -vf fps=1 ./img/%d.png");
+            //var inputArgs = "-i " + VideoFile + " -vf fps=8 -s 640x360 " + BaseImgPath +"%d.jpeg";
+            var inputArgs = "-sseof -10 -i " + VideoFile + " -s 640x360 " + BaseImgPath + "%d.jpeg";
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "ffmpeg.exe",
+                    Arguments = $"{inputArgs}",
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardInput = true
+                }
+            };
+            process.Start();
+
+            var ffmpegIn = process.StandardInput.BaseStream;
+            ffmpegIn.Flush();
+            ffmpegIn.Close();
+            process.WaitForExit();
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(BaseImgPath);
+            int count = dir.GetFiles().Length;
+            if (count > 0)
+            {
+                try
+                {
+                    CreateVideo(BaseImgPath, directory, filename, count);
+
+                }
+                catch 
+                {
+                }
+            }
+
+
+        }
+
+        public void CreateVideo(string BaseImgPath, string directory,string filename, int fileCount)
+        {
+
+            var imageCount = fileCount;
+            string outputVideoPath = BaseOutputVideoPath + directory + "/" ;
+            if (!Directory.Exists(outputVideoPath))
+                Directory.CreateDirectory(outputVideoPath);
+            using (var videoWriter = new VideoFileWriter())
+            {
+                videoWriter.Open(outputVideoPath + filename+".mp4", 640, 360, 25, VideoCodec.MPEG4,100000);
+                for (int imageFrame = 1; imageFrame < imageCount; imageFrame++)
+                {
+                    var imgPath = string.Format("{0}{1}.jpeg", BaseImgPath, imageFrame);
+                    using (Bitmap image = Bitmap.FromFile(imgPath) as Bitmap)
+                    {
+                        videoWriter.WriteVideoFrame(image);
+                    }
+                }
+                videoWriter.Close();
+                string[] filePaths = Directory.GetFiles(BaseImgPath);
+                foreach (string filePath in filePaths)
+                    File.Delete(filePath);
+                Console.WriteLine("Success Create Videos");
+
+            }
+        }
+
+    }
+}
---
+++ UploadVideoToFtp/Form1.cs
@@ -0,0 +1,151 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UploadVideoToFtp
+{
+    public partial class Form1 : Form
+    {
+        const string BaseOutputVideoPath = @"D:\ATCS\VideoOutput\";
+        private int _tick = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _tick++;
+            this.Text = _tick.ToString();
+            if (_tick == 1)
+            {
+                string[] lines = System.IO.File.ReadAllLines(@"config2.txt");
+
+                foreach (string line in lines)
+                {
+                    string[] words = line.Split('|');
+                    string directory = words[0];
+                    string filename = words[1];
+                    System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(BaseOutputVideoPath + directory + "/");
+                    if (dir.Exists)
+                    {
+                        int count = dir.GetFiles().Length;
+
+                        if (count > 0)
+                        {
+                            try
+                            {
+                                uploadtoftp(directory, filename);
+                                Console.WriteLine("Berhasil Upload file" + filename);
+                                label1.Text = "Berhasil Upload file " + filename;
+                            }
+                            catch
+                            {
+                                label1.Text = "Gagal Upload file " + filename;
+                                Console.WriteLine("GAGAL UPLOAD FILE" + filename);
+                            }
+                            try
+                            {
+                                publishRmq(directory, filename);
+                                label2.Text = "Success Publis Rmq " + filename;
+                            }
+                            catch
+                            {
+                                label2.Text = "Gagal Publis Rmq " + filename;
+                            }
+
+                        }
+                                                   
+                    }
+                    else {
+                        label1.Text = "CCTV Sedang dalam perbaikan" + filename;
+                    }
+
+                }
+                _tick = 0;
+            }
+
+        }
+
+        public void uploadtoftp(String directory, String filename)
+        {
+
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://atcs-lampung.pptik.id/atcs%20lampung/videos/" + directory + "/" + filename + ".mp4");
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.UseBinary = true;
+            request.UsePassive = true;
+            request.KeepAlive = true;
+            request.Credentials = new NetworkCredential("atcs_lampung", "atcslampung123!");
+
+            try
+            {
+
+                using (FileStream fs = File.OpenRead(BaseOutputVideoPath + directory + "/" + filename + ".mp4"))
+                {
+                    byte[] buffer = new byte[fs.Length];
+                    fs.Read(buffer, 0, buffer.Length);
+                    fs.Close();
+                    Stream requestStream = request.GetRequestStream();
+                    requestStream.Write(buffer, 0, buffer.Length);
+                    requestStream.Flush();
+                    requestStream.Close();
+                    FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                    //Console.WriteLine("Upload File Complete, status {0}", response.StatusDescription);
+                    response.Close();
+                    //Console.WriteLine("Success Upload Image to FTP");
+                }
+            }
+            catch {
+
+            }
+            
+
+           
+
+        }
+
+        private static void publishRmq(string path, string name)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.UserName = "atcs_lampung";
+            factory.Password = "atcslampung123!";
+            factory.VirtualHost = "/atcs_lampung";
+            factory.HostName = "rmq2.pptik.id";
+
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+
+
+                dynamic msg = new JObject();
+                msg.path = path;
+                msg.filename = name;
+
+                String jsonified = JsonConvert.SerializeObject(msg);
+                byte[] body = Encoding.UTF8.GetBytes(jsonified);
+
+                channel.BasicPublish(exchange: "amq.topic",
+                                     routingKey: "atcs_video",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", msg);
+                connection.Close();
+                channel.Close();
+
+            }
+        }
+    }
+}
